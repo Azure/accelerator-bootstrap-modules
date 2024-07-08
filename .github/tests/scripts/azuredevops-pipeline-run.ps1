475118573d16fd3a09d98fd291f9f9c235d187a2
Jared Holgate <jaredholgate@microsoft.com>
2024-07-08 19:24:56 +0100

    e2e test updates

--- .github/tests/scripts/azuredevops-pipeline-run.ps1
+++ .github/tests/scripts/azuredevops-pipeline-run.ps1
@@ -6,7 +6,8 @@ param (
     [switch]$skipDestroy = $false,
     [int]$maximumRetries = 10,
     [int]$retryCount = 0,
-    [int]$retryDelay = 10000
+    [int]$retryDelay = 10000,
+    [string]$iac
 )
 
 function Invoke-Pipeline {
@@ -15,6 +16,7 @@ function Invoke-Pipeline {
         [string]$projectName,
         [int]$pipelineId,
         [string]$pipelineAction = "",
+        [string]$iac,
         [hashtable]$headers
     )
     $pipelineDispatchUrl = "https://dev.azure.com/$organizationName/$projectName/_apis/pipelines/$pipelineId/runs?api-version=7.2-preview.1"
@@ -32,18 +34,35 @@ function Invoke-Pipeline {
             }
         } | ConvertTo-Json -Depth 100
     } else {
-        $pipelineDispatchBody = @{
-            "resources" = @{
-                "repositories" = @{
-                    "self" = @{
-                        "refName" = "refs/heads/main"
+        if($iac -eq "terraform") {
+            $pipelineDispatchBody = @{
+                "resources" = @{
+                    "repositories" = @{
+                        "self" = @{
+                            "refName" = "refs/heads/main"
+                        }
                     }
                 }
-            }
-            "templateParameters" = @{
-                "terraform_action" = $pipelineAction
-            }
-        } | ConvertTo-Json -Depth 100
+                "templateParameters" = @{
+                    "terraform_action" = $pipelineAction
+                }
+            } | ConvertTo-Json -Depth 100
+        }
+
+        if($iac -eq "bicep") {
+            $pipelineDispatchBody = @{
+                "resources" = @{
+                    "repositories" = @{
+                        "self" = @{
+                            "refName" = "refs/heads/main"
+                        }
+                    }
+                }
+                "templateParameters" = @{
+                    "destroy" = ($pipelineAction -eq "destroy").ToString().ToLower()
+                }
+            } | ConvertTo-Json -Depth 100
+        }
     }
 
     $result = Invoke-RestMethod -Method POST -Uri $pipelineDispatchUrl -Headers $headers -Body $pipelineDispatchBody -StatusCodeVariable statusCode -ContentType "application/json"
@@ -122,7 +141,7 @@ try {
 
     # Trigger the apply pipeline
     Write-Host "Triggering the $pipelineAction pipeline"
-    $pipelineRunId = Invoke-Pipeline -organizationName $organizationName -projectName $projectName -pipelineId $pipelineId -pipelineAction $pipelineAction -headers $headers
+    $pipelineRunId = Invoke-Pipeline -organizationName $organizationName -projectName $projectName -pipelineId $pipelineId -pipelineAction $pipelineAction -iac $iac -headers $headers
     Write-Host "$pipelineAction pipeline triggered successfully"
 
     # Wait for the apply pipeline to complete
@@ -139,7 +158,7 @@ try {
 
     # Trigger the destroy pipeline
     Write-Host "Triggering the $pipelineAction pipeline"
-    $pipelineRunId = Invoke-Pipeline -organizationName $organizationName -projectName $projectName -pipelineId $pipelineId -pipelineAction "destroy" -headers $headers
+    $pipelineRunId = Invoke-Pipeline -organizationName $organizationName -projectName $projectName -pipelineId $pipelineId -pipelineAction "destroy" -iac $iac -headers $headers
     Write-Host "$pipelineAction pipeline triggered successfully"
 
     # Wait for the apply pipeline to complete
