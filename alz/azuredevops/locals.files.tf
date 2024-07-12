d3cab96e9a5d8d6c431c484e9166e741840f72b0
Jared Holgate <jaredholgate@microsoft.com>
2024-07-12 14:18:08 +0100

    Add on demand folder support

--- alz/azuredevops/locals.files.tf
+++ alz/azuredevops/locals.files.tf
@@ -14,6 +14,9 @@ locals {
   script_files_all      = var.iac_type == "bicep" ? local.starter_module_config.deployment_files : []
   destroy_script_path   = var.iac_type == "bicep" ? local.starter_module_config.destroy_script_path : ""
 
+  # Get a list of on-demand folders
+  on_demand_folders = var.iac_type == "bicep" ? local.starter_module_config.on_demand_folders : []
+
   networking_type = var.iac_type == "bicep" ? jsondecode(file("${var.module_folder_path}/${var.bicep_parameters_file_path}")).NETWORK_TYPE : ""
   script_files = var.iac_type == "bicep" ? { for script_file in local.script_files_all : format("%03d", script_file.order) => {
     name                       = script_file.name
@@ -38,6 +41,7 @@ locals {
     } if contains(local.used_script_file_groups, script_file_group.name)
   }
 
+  # CI / CD Top Level Files
   cicd_files = { for pipeline_file in local.pipeline_files : "${local.target_folder_name}/${pipeline_file}" =>
     {
       content = templatefile("${local.pipeline_files_directory_path}/${pipeline_file}", {
@@ -51,30 +55,47 @@ locals {
     }
   }
 
+  # CI / CD Template Files
   cicd_template_files = { for pipeline_template_file in local.pipeline_template_files : "${local.target_folder_name}/${pipeline_template_file}" =>
     {
       content = templatefile("${local.pipeline_template_files_directory_path}/${pipeline_template_file}", {
-        agent_pool_configuration      = local.agent_pool_configuration
-        environment_name_plan         = local.resource_names.version_control_system_environment_plan
-        environment_name_apply        = local.resource_names.version_control_system_environment_apply
-        variable_group_name           = local.resource_names.version_control_system_variable_group
-        project_name                  = var.azure_devops_project_name
-        repository_name_templates     = local.repository_name_templates
-        service_connection_name_plan  = local.resource_names.version_control_system_service_connection_plan
-        service_connection_name_apply = local.resource_names.version_control_system_service_connection_apply
-        self_hosted_agent             = var.use_self_hosted_agents
-        script_files                  = local.script_files
-        destroy_script_path           = local.destroy_script_path
+        agent_pool_configuration       = local.agent_pool_configuration
+        environment_name_plan          = local.resource_names.version_control_system_environment_plan
+        environment_name_apply         = local.resource_names.version_control_system_environment_apply
+        variable_group_name            = local.resource_names.version_control_system_variable_group
+        project_name                   = var.azure_devops_project_name
+        repository_name_templates      = local.repository_name_templates
+        service_connection_name_plan   = local.resource_names.version_control_system_service_connection_plan
+        service_connection_name_apply  = local.resource_names.version_control_system_service_connection_apply
+        self_hosted_agent              = var.use_self_hosted_agents
+        script_files                   = local.script_files
+        destroy_script_path            = local.destroy_script_path
+        on_demand_folders              = local.on_demand_folders
+        on_demand_folder_repository    = var.on_demand_folder_repository
+        on_demand_folder_artifact_name = var.on_demand_folder_artifact_name
       })
     }
   }
 
+  # Build a map of module files and turn on the terraform backend block
   module_files = { for key, value in module.files.files : key =>
     {
       content = try(replace((file(value.path)), "# backend \"azurerm\" {}", "backend \"azurerm\" {}"), "unsupported_file_type")
     }
   }
-  module_files_supported    = { for key, value in local.module_files : key => value if value.content != "unsupported_file_type" && !endswith(key, "-cache.json") }
-  repository_files          = merge(local.cicd_files, local.module_files_supported, var.use_separate_repository_for_templates ? {} : local.cicd_template_files)
+
+  # Build a map of module files with types that are supported
+  module_files_supported = { for key, value in local.module_files : key => value if value.content != "unsupported_file_type" && !endswith(key, "-cache.json") && !endswith(key, var.bicep_config_file_path) }
+
+  # Build a list of files to exclude from the repository based on the on-demand folders
+  excluded_module_files = distinct(flatten([for exclusion in local.on_demand_folders :
+    [for key, value in local.module_files_supported : key if startswith(key, exclusion.target)]
+  ]))
+
+  # Filter out the excluded files
+  module_files_filtered = { for key, value in local.module_files_supported : key => value if !contains(local.excluded_module_files, key) }
+
+  # Create final maps of all files to be included in the repositories
+  repository_files          = merge(local.cicd_files, local.module_files_filtered, var.use_separate_repository_for_templates ? {} : local.cicd_template_files)
   template_repository_files = var.use_separate_repository_for_templates ? local.cicd_template_files : {}
 }
