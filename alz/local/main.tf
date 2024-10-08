72ad04bab8af6f988453dadd6b1e0e650178e837
Jared Holgate <jaredholgate@microsoft.com>
2024-10-08 17:24:38 +0100

    feat: add local script file for terraform (#48)
    * Bring into line with Bicep
    
    * linting

--- alz/local/main.tf
+++ alz/local/main.tf
@@ -55,3 +55,64 @@ resource "local_file" "alz" {
   content  = each.value.content
   filename = "${local.target_directory}/${each.key}"
 }
+
+locals {
+  command_with_azure_resources = <<COMMAND
+# Initialize the Terraform configuration
+terraform init `
+  -backend-config="resource_group_name=${local.resource_names.resource_group_state}" `
+  -backend-config="storage_account_name=${local.resource_names.storage_account}" `
+  -backend-config="container_name=${local.resource_names.storage_container}" `
+  -backend-config="key=terraform.tfstate" `
+  -backend-config="use_azuread_auth=true"
+
+# Run the Terraform plan
+terraform plan -out=tfplan
+
+# Review the Terraform plan
+terraform show tfplan
+
+Write-Host ""
+$deployApproved = Read-Host -Prompt "Type 'yes' and hit Enter to continue with the full deployment"
+Write-Host ""
+
+if($deployApproved -ne "yes") {
+  Write-Error "Deployment was not approved. Exiting..."
+  exit 1
+}
+
+# Apply the Terraform plan
+terraform apply tfplan
+
+COMMAND 
+
+  command_without_azure_resources = <<COMMAND
+# Initialize the Terraform configuration
+terraform init
+
+# Run the Terraform plan
+terraform plan -out=tfplan
+
+# Review the Terraform plan
+terraform show tfplan
+
+Write-Host ""
+$deployApproved = Read-Host -Prompt "Type 'yes' and hit Enter to continue with the full deployment"
+Write-Host ""
+
+if($deployApproved -ne "yes") {
+  Write-Error "Deployment was not approved. Exiting..."
+  exit 1
+}
+
+# Apply the Terraform plan
+terraform apply tfplan
+
+COMMAND
+}
+
+resource "local_file" "command" {
+  count    = var.iac_type == "terraform" ? 1 : 0
+  content  = var.create_bootstrap_resources_in_azure ? local.command_with_azure_resources : local.command_without_azure_resources
+  filename = "${local.target_directory}/scripts/deploy-local.ps1"
+}
