a79aecf7e83fb3ec2f93aa36d69f4ec8dcd56d05
sdeguchi <sdeguchi@microsoft.com>
2024-11-26 12:20:31 -0800

    feat: Adding top level management group name

--- modules/template_architecture_definition/variables.tf
+++ modules/template_architecture_definition/variables.tf
@@ -7,3 +7,21 @@ variable "architecture_definition_name" {
   type        = string
   description = "Name of the architecture definition"
 }
+
+variable "architecture_definition_template_path" {
+  type        = string
+  default     = ""
+  description = "The path to the architecture definition template file to use."
+}
+
+variable "architecture_definition_override_path" {
+  type        = string
+  default     = ""
+  description = "The path to the architecture definition file to use instead of the default."
+}
+
+variable "apply_alz_archetypes_via_architecture_definition_template" {
+  type        = bool
+  default     = true
+  description = "Toggles assignment of ALZ policies. True to deploy, otherwise false. (e.g true)"
+}
