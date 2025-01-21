49ab6690149c08147705b63b9df7cccd0c39add4
Jared Holgate <jaredholgate@microsoft.com>
2025-01-21 18:25:43 +0000

    fix: support azurerm v4 in local option

--- alz/local/locals.terraform.script.tf
+++ alz/local/locals.terraform.script.tf
@@ -1,5 +1,17 @@
 locals {
   command_base = <<COMMAND
+# Check and Set Subscription ID
+if($null -eq $env:ARM_SUBSCRIPTION_ID -or $env:ARM_SUBSCRIPTION_ID -eq "") {
+    Write-Verbose "Setting environment variable ARM_SUBSCRIPTION_ID"
+    $subscriptionId = $(az account show --query id -o tsv)
+    if($null -eq $subscriptionId -or $subscriptionId -eq "") {
+        Write-Error "Subscription ID not found. Please ensure you are logged in to Azure and have selected a subscription. Use 'az account show' to check."
+        return
+    }
+    $env:ARM_SUBSCRIPTION_ID = $subscriptionId
+    Write-Verbose "Environment variable ARM_SUBSCRIPTION_ID set to $subscriptionId"
+}
+
 # Run the Terraform plan
 terraform `
   -chdir="${var.root_module_folder_relative_path}" `
