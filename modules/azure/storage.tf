ddad91c346c8cab1a545f479ad93c644529e9e5e
Jared Holgate <jaredholgate@microsoft.com>
2025-03-03 16:59:55 +0000

    fix: storage data plan issues

--- modules/azure/storage.tf
+++ modules/azure/storage.tf
@@ -8,6 +8,9 @@ resource "azurerm_storage_account" "alz" {
   allow_nested_items_to_be_public = false
   shared_access_key_enabled       = false
   public_network_access_enabled   = var.use_private_networking && var.use_self_hosted_agents && !var.allow_storage_access_from_my_ip ? false : true
+  lifecycle {
+    ignore_changes = [ queue_properties, static_website ]
+  }
 }
 
 resource "azurerm_storage_account_network_rules" "alz" {
