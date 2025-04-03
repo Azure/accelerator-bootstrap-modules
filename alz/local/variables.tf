5686f326f16ad5c06ae7f5e9d98e5d2a22a864aa
Remo Leone Laudo <remoleone@icloud.com>
2025-04-03 09:39:58 -0400

    Updated GitHub and Local modules to match rbac role definitions with azuredevops

--- alz/local/variables.tf
+++ alz/local/variables.tf
@@ -208,6 +208,9 @@ variable "custom_role_definitions_terraform" {
           "Microsoft.Management/managementGroups/subscriptions/write",
           "Microsoft.Management/managementGroups/write",
           "Microsoft.Management/managementGroups/subscriptions/read",
+          "Microsoft.Management/managementGroups/settings/read",
+          "Microsoft.Management/managementGroups/settings/write",
+          "Microsoft.Management/managementGroups/settings/delete",
           "Microsoft.Authorization/*/read",
           "Microsoft.Resources/deployments/write",
           "Microsoft.Resources/deployments/exportTemplate/action"
@@ -222,6 +225,7 @@ variable "custom_role_definitions_terraform" {
         actions = [
           "Microsoft.Management/managementGroups/read",
           "Microsoft.Management/managementGroups/subscriptions/read",
+          "Microsoft.Management/managementGroups/settings/read",
           "Microsoft.Authorization/*/read",
           "Microsoft.Resources/deployments/write",
           "Microsoft.Resources/deployments/exportTemplate/action"
@@ -278,6 +282,9 @@ variable "custom_role_definitions_bicep" {
           "Microsoft.Management/managementGroups/subscriptions/write",
           "Microsoft.Management/managementGroups/write",
           "Microsoft.Management/managementGroups/subscriptions/read",
+          "Microsoft.Management/managementGroups/settings/read",
+          "Microsoft.Management/managementGroups/settings/write",
+          "Microsoft.Management/managementGroups/settings/delete",
           "Microsoft.Authorization/policyDefinitions/write",
           "Microsoft.Authorization/policySetDefinitions/write",
           "Microsoft.Authorization/policyAssignments/write",
@@ -302,6 +309,7 @@ variable "custom_role_definitions_bicep" {
         actions = [
           "Microsoft.Management/managementGroups/read",
           "Microsoft.Management/managementGroups/subscriptions/read",
+          "Microsoft.Management/managementGroups/settings/read",
           "Microsoft.Authorization/*/read",
           "Microsoft.Authorization/policyDefinitions/write",
           "Microsoft.Authorization/policySetDefinitions/write",
