39207086e73cf67ae0275a119ae4b950c1a20ab8
Jared Holgate <jaredholgate@microsoft.com>
2025-05-12 17:37:09 +0100

    feat: support looking up github user by username (#90)

--- modules/github/team.tf
+++ modules/github/team.tf
@@ -1,9 +1,23 @@
 locals {
-  approvers = [for user in data.github_organization.alz.users : {
+  approvers_by_email = [for approver in var.approvers : approver if strcontains(approver, "@")]
+  approvers_by_login = [for approver in var.approvers : approver if !strcontains(approver, "@")]
+
+  users_by_email = [for user in data.github_organization.alz.users : {
     login = user.login
     email = user.email
-  } if contains(var.approvers, user.email)]
-  invalid_approvers = setsubtract(var.approvers, local.approvers[*].email)
+  } if contains(local.approvers_by_email, user.email)]
+
+  users_by_login = [for user in data.github_organization.alz.users : {
+    login = user.login
+    email = user.email
+  } if contains(local.approvers_by_login, user.login)]
+
+  approvers = concat(local.users_by_email, local.users_by_login)
+
+  invalid_approvers_by_email = setsubtract(local.approvers_by_email, local.users_by_email[*].email)
+  invalid_approvers_by_login = setsubtract(local.approvers_by_login, local.users_by_login[*].login)
+
+  invalid_approvers = setunion(local.invalid_approvers_by_email, local.invalid_approvers_by_login)
 }
 
 resource "github_team" "alz" {
@@ -14,7 +28,7 @@ resource "github_team" "alz" {
   lifecycle {
     precondition {
       condition     = length(local.invalid_approvers) == 0
-      error_message = "At least one approver has not been supplied with a valid email. Invalid approvers: ${join(", ", local.invalid_approvers)}"
+      error_message = "At least one approver has not been supplied with a valid email or username. Invalid approvers by email: ${join(", ", local.invalid_approvers_by_email)}. Invalid approvers by username: ${join(", ", local.invalid_approvers_by_login)}."
     }
   }
 }
