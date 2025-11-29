7d59ac0cf58eacd8a4d9d314c8e373f05049d1b1
Jared Holgate <jaredholgate@microsoft.com>
2025-11-29 18:02:40 +0000

    loop cleanup

--- .github/tests/cleanup-scripts/cleanup_resouce_groups.ps1
+++ .github/tests/cleanup-scripts/cleanup_resouce_groups.ps1
@@ -3,17 +3,19 @@
 $filter = ""
 
 az account show
-$resourceGroups = @()
-if($filter -eq "")
-{
-    $resourceGroups = az group list | ConvertFrom-Json
-}
-else
-{
-    $resourceGroups = az group list --query "[?contains(name, '$filter')]" | ConvertFrom-Json
-}
+$resourceGroups = @("")
+while ($resourceGroups.Count -gt 0) {
+    if($filter -eq "")
+    {
+        $resourceGroups = az group list | ConvertFrom-Json
+    }
+    else
+    {
+        $resourceGroups = az group list --query "[?contains(name, '$filter')]" | ConvertFrom-Json
+    }
 
-$resourceGroups | ForEach-Object -Parallel {
-    Write-Host "Deleting resource group: $($_.name)"
-    az group delete --name $_.name --yes
-} -ThrottleLimit 10
+    $resourceGroups | ForEach-Object -Parallel {
+        Write-Host "Deleting resource group: $($_.name)"
+        az group delete --name $_.name --yes
+    } -ThrottleLimit 10
+}
