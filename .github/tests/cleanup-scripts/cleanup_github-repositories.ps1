ed61e6c6108a683a70d790d45e06cf53cc08ad91
Jared Holgate <jaredholgate@microsoft.com>
2025-11-29 18:01:38 +0000

    improve clean up jobs

--- .github/tests/cleanup-scripts/cleanup_github-repositories.ps1
+++ .github/tests/cleanup-scripts/cleanup_github-repositories.ps1
@@ -1,15 +1,20 @@
 # This file can be used to clean up GitHub repositories if there has been an issue with the End to End tests.
 # CAUTION: Make sure you are connected to the correct organization before running this script!
-$repos = gh repo list microsoft-azure-landing-zones-cd-tests --json name,owner | ConvertFrom-Json
+$filter = ""
 
-$repos | ForEach-Object -Parallel {
-    $match = "*229*"
-    $repoName = "$($_.owner.login)/$($_.name)"
-    
-    if($repoName -like $match)
-    {
-        Write-Host "Deleting repo: $repoName"
-        gh repo delete $repoName --yes
-    
-    }
-} -ThrottleLimit 10
+$repos = @("")
+while ($repos.Count -gt 0) {
+    $repos = gh repo list microsoft-azure-landing-zones-cd-tests --json name,owner | ConvertFrom-Json
+
+    $repos | ForEach-Object -Parallel {
+        $match = $using:filter
+        $repoName = "$($_.owner.login)/$($_.name)"
+
+        if($match -eq "" -or $repoName -like $match)
+        {
+            Write-Host "Deleting repo: $repoName"
+            gh repo delete $repoName --yes
+
+        }
+    } -ThrottleLimit 10
+}
--- .github/tests/cleanup-scripts/cleanup_resouce_groups.ps1
+++ .github/tests/cleanup-scripts/cleanup_resouce_groups.ps1
@@ -1,7 +1,17 @@
 # This file can be used to clean up Resource Groups if there has been an issue with the End to End tests.
 # CAUTION: Make sure you are connected to the correct subscription before running this script!
+$filter = ""
+
 az account show
-$resourceGroups = az group list --query "[?!contains(name, '713-')]" | ConvertFrom-Json
+$resourceGroups = @()
+if($filter -eq "")
+{
+    $resourceGroups = az group list | ConvertFrom-Json
+}
+else
+{
+    $resourceGroups = az group list --query "[?contains(name, '$filter')]" | ConvertFrom-Json
+}
 
 $resourceGroups | ForEach-Object -Parallel {
     Write-Host "Deleting resource group: $($_.name)"
