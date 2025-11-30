807387b2b0442d2bac610feafe8681887db548db
Jared Holgate <jaredholgate@microsoft.com>
2025-11-30 15:54:10 +0000

    fix scripts

--- .github/tests/cleanup-scripts/cleanup_github-repositories.ps1
+++ .github/tests/cleanup-scripts/cleanup_github-repositories.ps1
@@ -1,10 +1,14 @@
 # This file can be used to clean up GitHub repositories if there has been an issue with the End to End tests.
 # CAUTION: Make sure you are connected to the correct organization before running this script!
-$filter = "*725*"
+$filter = ""
 
 $repos = @("")
 while ($repos.Count -gt 0) {
-    $repos = gh repo list microsoft-azure-landing-zones-cd-tests --json name,owner | ConvertFrom-Json | Where-Object { $_.name -like $filter -or $filter -eq "" }
+    $repos = gh repo list microsoft-azure-landing-zones-cd-tests --json name,owner | ConvertFrom-Json
+    if($filter -ne "")
+    {
+        $repos = $repos | Where-Object { $_.name -like "*$filter*" }
+    }
 
     $repos | ForEach-Object -Parallel {
         $repoName = "$($_.owner.login)/$($_.name)"
