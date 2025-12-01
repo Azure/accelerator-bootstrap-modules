45f01abdbcf67c62148aab8d0b80dccb3350747a
Jared Holgate <jaredholgate@microsoft.com>
2025-12-01 12:04:32 +0000

    add check

--- .github/tests/cleanup-scripts/cleanup_resouce_groups.ps1
+++ .github/tests/cleanup-scripts/cleanup_resouce_groups.ps1
@@ -1,6 +1,10 @@
 # This file can be used to clean up Resource Groups if there has been an issue with the End to End tests.
 # CAUTION: Make sure you are connected to the correct subscription before running this script!
 $managementGroupFilter = "alz-r"
+if($managementGroupFilter -eq "")
+{
+    throw "Please set a management group filter to avoid disaster!"
+}
 $subscriptionFilter = ""
 
 $managementGroups = @(
