05dc11ff1bcd071680ba5bbfa2ef57d7e506bafe
Jared Holgate <jaredholgate@microsoft.com>
2025-12-02 11:11:53 +0000

    fix linting

--- modules/azure/locals.tf
+++ modules/azure/locals.tf
@@ -3,6 +3,5 @@ locals {
 }
 
 locals {
-  subscription_ids          = { for subscription_id in distinct(var.target_subscriptions) : subscription_id => subscription_id }
-  subscription_resource_ids = [for key, value in data.azurerm_subscription.alz : value.id]
+  subscription_ids = { for subscription_id in distinct(var.target_subscriptions) : subscription_id => subscription_id }
 }
